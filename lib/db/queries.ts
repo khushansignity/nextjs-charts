cd6f236dabe6f1a3b73c43a8a40bcd58876d15c0
Khushanpreet Singh <khushanpreet@signitysolutions.com>
2025-04-22 17:45:52 +0530

    updated query

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -1,6 +1,7 @@
+import { asc } from "drizzle-orm";
 import { db } from "./drizzle";
 import { balances } from "./schema";
 
 export async function getChartData() {
-    return await db.select().from(balances);
+    return await db.select().from(balances).orderBy(asc(balances.date));
 }
